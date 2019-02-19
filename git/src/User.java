9d21d3a48aff022bb349fd0090c4c7a157ebc4ba
wuxiang <2450533591@qq.com>
2019-02-19 09:29:48 +0800

    添加user工程到git

---
+++ git/src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello,java!");
+        System.out.println("hello,java!");
+        System.out.println("hello,java!");
+        System.out.println("hello,java!");
+        System.out.println("hello,java!");
+    }
+}
